86404fed469a67dd2c007fb049375d2b02cd10a7
Bailey Garrett <baileyagarrett@gmail.com>
2020-02-29 16:27:56 -0600

    fixing syntax for schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,13 @@
-### Schema
-
 CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers
 (
-	id int NOT NULL AUTO_INCREMENT,
-	burger_name varchar(255) NOT NULL,
+    id int NOT NULL
+    IDENTITY (1, 1),
+	burger_name varchar
+    (255) NOT NULL,
     devoured BOOLEAN NOT NULL,
-	PRIMARY KEY (id)
+	PRIMARY KEY
+    (id)
 );
