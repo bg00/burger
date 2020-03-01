cba0a14ae9ff2cd36166bcc55ffb100ac7e06890
Bailey Garrett <baileyagarrett@gmail.com>
2020-03-01 12:34:08 -0600

    adding config files and update to schema format

--- db/schema.sql
+++ db/schema.sql
@@ -6,8 +6,8 @@ CREATE TABLE burgers
     id int NOT NULL
     AUTO_INCREMENT,
 	burger_name varchar
-    255 NOT NULL,
+    (255) NOT NULL,
     devoured BOOLEAN,
 	PRIMARY KEY
-    id
+    (id)
 );
